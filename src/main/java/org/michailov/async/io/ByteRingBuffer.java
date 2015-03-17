c97630d6b5833f18972d1748d67b3dbb86b17dab
Zlatko Michailov <zlatko@michailov.org>
2015-03-16 22:08:22 -0700

    Added javadoc for RingBuffer and ByteRingBuffer. NEXT: CharRingBuffer.

--- src/main/java/org/michailov/async/io/ByteRingBuffer.java
+++ src/main/java/org/michailov/async/io/ByteRingBuffer.java
@@ -1,25 +1,56 @@
 package org.michailov.async.io;
 
+/**
+ * This is a concrete implementation of {@link RingBuffer} of a byte[].
+ * 
+ * @author Zlatko Michailov
+ */
 public class ByteRingBuffer extends RingBuffer {
 
     private static final int NOT_AVAILABLE = -1;
     
-    byte[] _buffer;
+    private final byte[] _buffer;
     
+    /**
+     * Constructs a ring buffer over an implicitly allocated byte[].
+     *  
+     * @param   capacity    Capacity of the implicit byte[].
+     */
     public ByteRingBuffer(int capacity) {
         this(new byte[capacity]);
     }
     
+    /**
+     * Constructs a ring buffer over an existing byte[].
+     * This overload is useful when the caller wants to reuse existing arrays
+     * to avoid unnecessary heap allocations.
+     *  
+     * @param   buffer    An existing byte[].
+     */
     public ByteRingBuffer(byte[] buffer) {
         super(buffer.length);
         
         _buffer = buffer;
     }
     
+    /**
+     * Gets the underlying byte[].
+     * This may be passed to buffer copying APIs along with {@link RingBuffer#getAvailableToReadStraight}
+     * or {@link RingBuffer#getAvailableToWriteStraight}. 
+     * 
+     * @return  The underlying byte[].
+     */
     public byte[] getBuffer() {
         return _buffer;
     }
     
+    /**
+     * Gets the byte at a given relative position ahead of the current virtual read position.
+     *  
+     * @param   delta   A relative position ahead of the virtual read position to peek at.
+     * @return          The value of the byte at the peek position or <b>-1</b> if <i>delta</i>
+     *                  is greater than {@link RingBuffer#getAvailableToRead}. 
+     */
     public int peek(int delta) {
         if (delta < 0) {
             throw new IllegalArgumentException("Argument delta may not be negative.");
@@ -34,6 +65,11 @@ public class ByteRingBuffer extends RingBuffer {
         return b;
     }
     
+    /**
+     * Reads the byte at the virtual read position, and advances the virtual read position by 1.
+     * 
+     * @return      The byte at the virtual read position or <b>-1</b> if nothing is available to read.
+     */
     public int read() {
         int p = peek(0);
         if (p == NOT_AVAILABLE) {
@@ -44,6 +80,12 @@ public class ByteRingBuffer extends RingBuffer {
         return p;
     }
     
+    /**
+     * Writes a byte at the virtual write position, and advances the virtual write position by 1.
+     * 
+     * @param   b   Byte to be written.   
+     * @return      The byte that was written or <b>-1</b> if no space was available to write.
+     */
     public int write(byte b) {
         if (getAvailableToWrite() <= 0) {
             return NOT_AVAILABLE;
--- src/main/java/org/michailov/async/io/RingBuffer.java
+++ src/main/java/org/michailov/async/io/RingBuffer.java
@@ -1,5 +1,20 @@
 package org.michailov.async.io;
 
+/**
+ * This class defines an abstract ring buffer functionality where a virtual read position 
+ * and a virtual write position chase each other around a buffer length.
+ * The type of the items in the buffer is irrelevant to this functionality.
+ * <p>
+ * The virtual write position is always "ahead" of the virtual read position. 
+ * That is because something has to be written first before it could be read.
+ * <p>
+ * <u>Thread safety</u>: This functionality is safe to be used by one "reader" thread
+ * and one "writer" thread concurrently as long as the reader thread only updates
+ * the virtual read position (it's OK to get the virtual write position) and the "writer"
+ * thread only updates the virtual write position (it's OK to get the virtual read position.)
+ * 
+ * @author Zlatko Michailov
+ */
 public abstract class RingBuffer {
 
     private final long _bufferLength;
@@ -7,6 +22,11 @@ public abstract class RingBuffer {
     private volatile long _virtualWritePosition;
     private volatile boolean _eof;
     
+    /**
+     * Constructor to be called from concrete implementations.
+     * 
+     * @param bufferLength      Length of the underlying buffer.
+     */
     protected RingBuffer(int bufferLength) {
         _bufferLength = bufferLength;
         _virtualReadPosition = 0;
@@ -14,56 +34,145 @@ public abstract class RingBuffer {
         _eof = false;
     }
     
+    /**
+     * Gets the EOF state of this ring buffer.
+     * The EOF state is explicitly set by a writing client. 
+     * 
+     * @return  ture iff no more bytes will be written to this ring buffer.
+     */
     public boolean getEOF() {
         return _eof;
     }
     
+    /**
+     * Sets the EOF state of this ring buffer.
+     * The EOF state is explicitly set by a writing client. 
+     */
     public void setEOF() {
         _eof = true;
     }
     
+    /**
+     * Gets the length of the underlying buffer.
+     * 
+     * @return  The length of the underlying buffer.
+     */
     public int getBufferLength() {
         return (int)_bufferLength;
     }
     
+    /**
+     * Gets the current virtual read position.
+     * 
+     * @return  The current virtual read position.
+     */
     public int getReadPosition() {
         return (int)(_virtualReadPosition % _bufferLength);
     }
     
+    /**
+     * Gets the current virtual write position.
+     * 
+     * @return  The current virtual write position.
+     */
     public int getWritePosition() {
         return (int)(_virtualWritePosition % _bufferLength);
     }
     
+    /**
+     * Gets the total count of read items.
+     * 
+     * @return  The total count of read items.
+     */
     public long getTotalReadCount() {
         return _virtualReadPosition;
     }
     
+    /**
+     * Gets the total count of written items.
+     * 
+     * @return  The total count of written items.
+     */
     public long getTotalWriteCount() {
         return _virtualWritePosition;
     }
     
+    /**
+     * Gets the count of items that are available to be read.
+     * Reading these items may include a flip over, i.e. these items can be read
+     * one by one, but not necessarily all at once.
+     * To get the items that can be read at once, see {@link #getAvailableToReadStraight}.
+     * 
+     * @return  The count of items available to be read one by one.
+     */
     public int getAvailableToRead() {
         return (int)(_virtualWritePosition - _virtualReadPosition);
     }
     
+    /**
+     * Gets the count of items that are available to be read at once. 
+     * This method is useful to be used with APIs that do buffer copying.
+     * This count is less than or equal to the count returned from {@link #getAvailableToRead}.
+     * 
+     * @return  The count of items available to be read at once.
+     */
     public int getAvailableToReadStraight() {
         return Math.min(getAvailableToRead(), (int)(_bufferLength - (_virtualReadPosition % _bufferLength)));
     }
     
+    /**
+     * Gets the count of items that are available to be written.
+     * Writing these items may include a flip over, i.e. these items can be written
+     * one by one, but not necessarily all at once.
+     * To get the items that can be written at once, see {@link #getAvailableToWriteStraight}.
+     * 
+     * @return  The count of items available to be written one by one.
+     */
     public int getAvailableToWrite() {
         return (int)(_bufferLength - (_virtualWritePosition - _virtualReadPosition));
     }
     
+    /**
+     * Gets the count of items that are available to be written at once. 
+     * This method is useful to be used with APIs that do buffer copying.
+     * This count is less than or equal to the count returned from {@link #getAvailableToWrite}.
+     * 
+     * @return  The count of items available to be written at once.
+     */
     public int getAvailableToWriteStraight() {
         return Math.min(getAvailableToWrite(), (int)(_bufferLength - (_virtualWritePosition % _bufferLength)));
     }
     
+    /**
+     * This method must be called to indicate a successful read operation.
+     * If multiple items are read one by one, it is not necessary to advance the read position
+     * after each item. That could be done once for all read items.
+     * <p> 
+     * An attempt to advance the read position beyond the value returned by {@link #getAvailableToRead}
+     * limits the attempt to the value of {@link #getAvailableToRead}.
+     * That is not an exceptional case.
+     * 
+     * @param   delta   The count of virtual notches by which to increment the virtual read position. 
+     * @return          The minimum of the requested <i>delta</i> and the value of {@link #getAvailableToRead}. 
+     */
     public int advanceReadPosition(int delta) {
         int availableDelta = getAvailableDeltaToAdvance(delta, getAvailableToRead());
         _virtualReadPosition += availableDelta;
         return availableDelta;
     }
     
+    /**
+     * This method must be called to indicate a successful write operation.
+     * If multiple items are written one by one, it is not necessary to advance the write position
+     * after each item. That could be done once for all written items.
+     * <p> 
+     * An attempt to advance the write position beyond the value returned by {@link #getAvailableToWrite}
+     * limits the attempt to the value of {@link #getAvailableToWrite}.
+     * That is not an exceptional case.
+     * 
+     * @param   delta   The count of virtual notches by which to increment the virtual write position. 
+     * @return          The minimum of the requested <i>delta</i> and the value of {@link #getAvailableToWrite}. 
+     */
     public int advanceWritePosition(int delta) {
         int availableDelta = getAvailableDeltaToAdvance(delta, getAvailableToWrite());
         _virtualWritePosition += availableDelta;
