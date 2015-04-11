3aabb3739fcc22c7249434fb95c290de74ea3f63
Zlatko Michailov <zlatko@michailov.org>
2015-04-11 10:59:02 -0700

    Add javadoc for Executor.

--- src/main/java/org/michailov/async/Executor.java
+++ src/main/java/org/michailov/async/Executor.java
@@ -2,18 +2,34 @@ package org.michailov.async;
 
 import java.util.concurrent.*;
 
+/**
+ * This class abstracts away the mess in Java aync scheduling.  
+ * 
+ * @author Zlatko Michailov
+ */
 class Executor {
     
     private static final ForkJoinPool MAIN_THREAD_POOL = ForkJoinPool.commonPool();
     private static final int DELAY_THREAD_COUNT = 1;
     private static final ScheduledThreadPoolExecutor DELAY_THREAD_POOL = new ScheduledThreadPoolExecutor(DELAY_THREAD_COUNT);
     
-    static void execute(Runnable task) {
-        MAIN_THREAD_POOL.execute(task);
+    /**
+     * Schedules an operation for an immediate execution on the main thread pool.
+     * 
+     * @param   runnable The operation to be executed.
+     */
+    static void execute(Runnable runnable) {
+        MAIN_THREAD_POOL.execute(runnable);
     }
 
-    static void executeAfter(Runnable task, long delayMillis) {
-        DELAY_THREAD_POOL.schedule(() -> execute(task), delayMillis, TimeUnit.MILLISECONDS);
+    /**
+     * Schedules an operation for a delayed execution on the main thread pool.
+     * 
+     * @param   runnable    The operation to be executed.
+     * @param   delayMillis The amount of milliseconds to delay the execution.
+     */
+    static void executeAfter(Runnable runnable, long delayMillis) {
+        DELAY_THREAD_POOL.schedule(() -> execute(runnable), delayMillis, TimeUnit.MILLISECONDS);
     }
 
 }
